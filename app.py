eab4c539275f7d0d461cfb6910ae920c88cf8237
SerBABA <dan.r0n3n@gmail.com>
2020-10-02 02:57:12 +1300

    first commit!

---
+++ app.py
@@ -0,0 +1,11 @@
+
+
+class Vault():
+
+    def __init__(self):
+        self.DATABASE_FILE_NAME = 'vault.db'
+        self.MAX_GUESS_ATTEMPTS = 3
+        self.MINIMUM_PASSWORD_LENGTH = 4
+
+        self.db = None
+        self.is_owner = False
---
+++ model.py
@@ -0,0 +1,8 @@
+import sqlite3
+
+
+class Model:
+
+    def __init__(self, db_name):
+        self.DB_NAME = db_name
+        
